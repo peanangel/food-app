d8b96d9a9e07cff9672b5a4d13dbe36d093349b6
peanangel <aranya.sangthong03@gmail.com>
2025-03-19 17:22:43 +0700

    Update my_app_home_screen.dart

--- lib/Views/my_app_home_screen.dart
+++ lib/Views/my_app_home_screen.dart
@@ -16,56 +16,76 @@ class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kbackgroundColor,
-      body: SafeArea(child: SingleChildScrollView(child: Column(
-        children: [
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 15)
-          ,child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-            headerParts(),
-            mySearchBar(),
-            const BannerToExplore(),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 20),
-            child: Text("Categories",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    headerParts(),
+                    mySearchBar(),
+                    const BannerToExplore(),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "Categories",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
-          ),)
-        ],
-      ),)),
+          ),
+        ),
+      ),
     );
   }
 
   Padding mySearchBar() {
-    return Padding(padding: EdgeInsets.symmetric(vertical: 22),
-          child: TextField(
-            decoration: InputDecoration(filled: true,
-            prefixIcon: const Icon(Iconsax.search_normal),
-            fillColor: Colors.white,
-            border: InputBorder.none,hintText: "Search any recipes",
-            hintStyle: const TextStyle(color: Colors.grey),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide.none
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide.none
-            ),
-            ),
-          )
-          );
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 22),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          prefixIcon: const Icon(Iconsax.search_normal),
+          fillColor: Colors.white,
+          border: InputBorder.none,
+          hintText: "Search any recipes",
+          hintStyle: const TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
   }
 
   Row headerParts() {
     return Row(
-            children: [
-              const Text("What are you\ncooking today?",
-              style: TextStyle(fontSize: 32,
-              fontWeight: FontWeight.bold,height: 1),),
-              const Spacer(),
-              MyIconButton(icon: Iconsax.notification, pressed: (){}),
-              
-
-            ],
-          );
+      children: [
+        const Text(
+          "What ..are you\ncooking today?",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            height: 1,
+          ),
+        ),
+        const Spacer(),
+        MyIconButton(icon: Iconsax.notification, pressed: () {}),
+      ],
+    );
   }
 }
